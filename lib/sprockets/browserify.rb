07bdf3f297d93165a5b521d2d4f47f20752c41f7
Jan Varwig <jan@varwig.org>
2013-03-07 21:31:01 +0100

    Fix NODE_PATH problem for module-deps

--- lib/sprockets/browserify.rb
+++ lib/sprockets/browserify.rb
@@ -1,6 +1,7 @@
 require 'sprockets'
 require 'tilt'
 require 'pathname'
+require 'shellwords'
 
 module Sprockets
   # Postprocessor that runs the computed source of Javascript files
@@ -13,7 +14,7 @@ module Sprockets
 
     def evaluate(scope, locals, &block)
       if (scope.pathname.dirname+'package.json').exist?
-        deps = `node -e "mdeps=require('module-deps'),through=require('through');mdeps('#{scope.pathname}').pipe(through(function(d){ console.log(d.id); }))"`
+        deps = `NODE_PATH=#{Shellwords.shellescape((gem_dir+'node_modules').to_s)} node -e "mdeps=require('module-deps'),through=require('through');mdeps('#{scope.pathname}').pipe(through(function(d){ console.log(d.id); }))"`
         deps.lines.reject{|line| line =~ /module-deps/}.drop(1).each{|path| scope.depend_on path.strip}
         # TODO also throw an error if browserify fucks up
         @output ||= `#{browserify_executable} -d #{scope.pathname}`
@@ -24,8 +25,12 @@ module Sprockets
 
   protected
 
+    def gem_dir
+      @gem_dir ||= Pathname.new(__FILE__).dirname + '../..'
+    end
+
     def browserify_executable
-      @browserify_executable ||= Pathname.new(__FILE__).dirname + '../../node_modules/.bin/browserify'
+      @browserify_executable ||= gem_dir + 'node_modules/.bin/browserify'
     end
 
   end
